18c2d4ab0674023ad9ffbdebc08be83ed2195240
cwc329 <iaiminwtg@gmail.com>
2021-07-18 22:06:05 +0800

    setup ci/cd

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,2 +1,5 @@
-require('dotenv').config();
-export const BASE_URL = process.env.API_URL;
+require('dotenv-expand')(require('dotenv').config());
+
+export const BASE_URL = process.env.REACT_APP_API_URL;
+
+console.log(BASE_URL);
